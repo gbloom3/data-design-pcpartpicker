5d78853ab6912d4086e81d130667d93d8031358c
Greg Bloom <gbloom3@cnm.edu>
2016-10-12 11:03:50 -0600

    Added index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Data Design: PC Part Picker</title>
+	</head>
+	<body>
+		<header>
+			<h1>Data Design: PC Part Picker</h1>
+		</header>
+	</body>
+</html>
