87aff4b988140b133b01f9e3506dd5baea697cd1
Greg Bloom <gbloom3@cnm.edu>
2016-10-12 11:43:50 -0600

    Added index.php

--- index.php
+++ index.php
@@ -9,4 +9,21 @@
 			<h1>Data Design: PC Part Picker</h1>
 		</header>
 	</body>
+	<main>
+		<h2>Persona</h2>
+		<hr/>
+		<p>
+			Computer users of all experience levels planning out a new computer build or upgrades to an existing build. In this case specifically, users in the market for a new CPU.
+		</p>
+		<h2>Use Case</h2>
+		<hr/>
+		<p>
+
+		</p>
+		<h2>Entities</h2>
+		<hr/>
+		<p>
+
+		</p>
+	</main>
 </html>
