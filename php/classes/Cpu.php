9c88a535de4924e870b310433c189407769b5bc5
Greg Bloom <gbloom3@cnm.edu>
2016-10-19 15:15:54 -0600

    Added accessor functions for all state variables

--- php/classes/Cpu.php
+++ php/classes/Cpu.php
@@ -15,15 +15,20 @@ class Cpu{
 	 **/
 	private $cpuId;
 	/**
+	 * manufacturer for this cpu
+	 * @var string $cpuManufacturer
+	 **/
+	private $cpuManufacturer;
+	/**
 	 * model name for this cpu
 	 * @var string $cpuModelName
 	 **/
 	private $cpuModelName;
 	/**
-	 * manufacturer for this cpu
-	 * @var string $cpuManufacturer
+	 * model number for this cpu
+	 * @var string $cpuModelNumber
 	 **/
-	private $cpuManufacturer;
+	private $cpuModelNumber;
 	/**
 	 * data width for this cpu; ie 32-bit, 64-bit
 	 * @var int $cpuDataWidth
@@ -69,5 +74,118 @@ class Cpu{
 	 * @var bool $cpuIsOnboardGraphics
 	 **/
 	private $cpuIsOnboardGraphics;
+
+	//Constructor goes here
+
+	/**
+	 * accessor method for cpuId
+	 * @return int|null value of cpuId
+	 **/
+	public function getCpuId() {
+		return $this->cpuId;
+	}
+	/**
+	 * accessor method for cpuManufacturer
+	 * @return string value of cpuManufacturer
+	 **/
+	public function getCpuManufacturer() {
+		return $this->cpuManufacturer;
+	}
+	/**
+	 * accessor method for cpuModelName
+	 * @return string value of cpuModelName
+	 **/
+	public function getCpuModelName() {
+		return $this->cpuModelName;
+	}
+	/**
+	 * accessor method for cpuModelNumber
+	 * @return string value of cpuModelNumber
+	 **/
+	public function getCpuModelNumber() {
+		return $this->cpuModelNumber;
+	}
+	/**
+	 * accessor method for cpuDataWidth
+	 * @return int|null value of cpuDataWidth
+	 **/
+	public function getCpuDataWidth() {
+		return $this->cpuDataWidth;
+	}
+	/**
+	 * accessor method for cpuSocket
+	 * @return string value of cpuSocket
+	 **/
+	public function getCpuSocket() {
+		return $this->cpuSocket;
+	}
+	/**
+	 * accessor method for cpuOperatingFrequency
+	 * @return float value of cpuOperatingFrequency
+	 **/
+	public function getCpuOperatingFrequency() {
+		return $this->cpuOperatingFrequency;
+	}
+	/**
+	 * accessor method for cpuTurboFrequency
+	 * @return float value of cpuTurboFrequency
+	 **/
+	public function getCpuTurboFrequency() {
+		return $this->cpuTurboFrequency;
+	}
+	/**
+	 * accessor method for cpuCores
+	 * @return int|null value of cpuCores
+	 **/
+	public function getCpuCores() {
+		return $this->cpuCores;
+	}
+	/**
+	 * accessor method for cpuCache
+	 * @return string value of cpuCache
+	 **/
+	public function getCpuCache() {
+		return $this->cpuCache;
+	}
+	/**
+	 * accessor method for cpuIsCoolerIncluded
+	 * @return bool value of cpuIsCoolerIncluded
+	 **/
+	public function getCpuIsCoolerIncluded() {
+		return $this->cpuIsCoolerIncluded;
+	}
+	/**
+	 * accessor method for cpuIsHyperThreading
+	 * @return bool value of cpuIsHyperThreading
+	 **/
+	public function getCpuIsHyperThreading() {
+		return $this->cpuIsHyperThreading;
+	}
+	/**
+	 * accessor method for cpuIsOnboardGraphics
+	 * @return bool value of cpuIsOnboardGraphics
+	 **/
+	public function getCpuIsOnboardGraphics() {
+		return $this->cpuIsOnboardGraphics;
+	}
+	/**
+	 * mutator method for cpuId
+	 *
+	 * @param int|null $newCpuId new value of cpu id
+	 * @throws \RangeException if $newCpuId is negative
+	 * @throws \TypeError if $newCpuId is not an integer
+	 **/
+	public function setCpuId(int $newCpuId = null) {
+		// base case: if the cpu id is null, this is a new cpu without an id from mySQL
+		if($newCpuId === null) {
+			$this->cpuId = null;
+			return;
+		}
+		//verify the cpu id is positive
+		if($newCpuId <= 0){
+			throw (new \RangeException("cpu id is not positive"));
+		}
+		// converts and stores the cpu id
+		$this->cpuId = $newCpuId;
+	}
 }
-?>
--- php/classes/Profile.php
+++ php/classes/Profile.php
@@ -40,5 +40,69 @@ class Profile{
 	 * @var string $profileAbout
 	 */
 	private $profileAbout;
+
+	// constructor goes here
+
+	/**
+	 * accessor method for profileId
+	 * @return int|null value of profileId
+	 **/
+	public function getProfileId() {
+		return $this->profileId;
+	}
+	/**
+	 * accessor method for profileImage
+	 * @return string of profileImage
+	 **/
+	public function getProfileImage() {
+		return $this->profileImage;
+	}
+	/**
+	 * accessor method for profileName
+	 * @return string of profileName
+	 **/
+	public function getProfileName() {
+		return $this->profileName;
+	}
+	/**
+	 * accessor method of profileDateCreated
+	 * @return DateTime of profileDateCreated
+	 **/
+	public function  getProfileDateCreated() {
+		return $this->profileDateCreated;
+	}
+	/**
+	 * accessor method of profileEmail
+	 * @return string of profileEmail
+	 **/
+	public function  getProfileEmail() {
+		return $this->profileEmail;
+	}
+	/**
+	 * accessor method of profileAbout
+	 * @return string of profileAbout
+	 **/
+	public function  getProfileAbout() {
+		return $this->profileAbout;
+	}
+	/**
+	 * mutator method for profileId
+	 *
+	 * @param int|null $newProfileId new value of profile id
+	 * @throws \RangeException if $newProfileId is negative
+	 * @throws \TypeError if $newProfileId is not an integer
+	 **/
+	public function setProfileId(int $newProfileId = null) {
+		// base case: if the profile id is null, this is a new profile without an id from mySQL
+		if($newProfileId === null) {
+			$this->profileId = null;
+			return;
+		}
+		//verify the profile id is positive
+		if($newProfileId <= 0){
+			throw (new \RangeException("profile id is not positive"));
+		}
+		// converts and stores the profile id
+		$this->profileId = $newProfileId;
+	}
 }
-?>
